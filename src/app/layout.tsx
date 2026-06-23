84c2fb3b76a6667b7804f5ae8784b890b217d7a4
Augusto Gomez <agomezsantos95@gmail.com>
2026-06-23 23:18:00 +0200

    Add navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import './globals.scss';
 import { Providers } from './providers';
+import { Navbar } from '@/components/navbar/Navbar';
+import { Container } from '@/components/container/Container';
 
 export const metadata: Metadata = {
   title: 'Mobile Store',
@@ -15,7 +17,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Providers>{children}</Providers>
+        <Container>
+          <Providers>
+            <Navbar />
+            <main>
+              {/* <Container>{children}</Container> */}
+              {children}
+            </main>
+          </Providers>
+        </Container>
       </body>
     </html>
   );
