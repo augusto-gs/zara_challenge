81eee8dd51fcc910c5445a300bad999d932df9fa
Augusto Gomez <agomezsantos95@gmail.com>
2026-06-23 13:19:17 +0200

    Add tests for mobile store api functions

---
+++ src/mocks/phoneItemMocks.ts
@@ -0,0 +1,44 @@
+import { PhoneItem } from '@/lib/types/phones';
+
+export const phoneItemListMock: PhoneItem[] = [
+  {
+    id: 'SMG-S24U',
+    brand: 'Samsung',
+    name: 'Galaxy S24 Ultra',
+    basePrice: 1329,
+    imageUrl:
+      'http://prueba-tecnica-api-tienda-moviles.onrender.com/images/SMG-S24U-titanium-violet.webp',
+  },
+  {
+    id: 'SMG-A25',
+    brand: 'Samsung',
+    name: 'Galaxy A25 5G',
+    basePrice: 239,
+    imageUrl:
+      'http://prueba-tecnica-api-tienda-moviles.onrender.com/images/SMG-A25-negro.webp',
+  },
+  {
+    id: 'GPX-8A',
+    brand: 'Google',
+    name: 'Pixel 8a',
+    basePrice: 459,
+    imageUrl:
+      'http://prueba-tecnica-api-tienda-moviles.onrender.com/images/GPX-8A-obsidiana.webp',
+  },
+  {
+    id: 'APL-I15PM',
+    brand: 'Apple',
+    name: 'iPhone 15 Pro Max',
+    basePrice: 1319,
+    imageUrl:
+      'http://prueba-tecnica-api-tienda-moviles.onrender.com/images/APL-I15PM-titanio-negro.webp',
+  },
+  {
+    id: 'OPP-A18',
+    brand: 'OPPO',
+    name: 'A18',
+    basePrice: 99,
+    imageUrl:
+      'http://prueba-tecnica-api-tienda-moviles.onrender.com/images/OPP-A18-azul-brillante.webp',
+  },
+];
