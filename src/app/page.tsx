475a60dd456d48a55681fa776423cd141858fac3
Augusto Gomez <agomezsantos95@gmail.com>
2026-06-25 10:32:29 +0200

    Add loading bar and error view component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import { getPhones } from '@/lib/api/phones';
 import { PhoneList } from '@/components/phone-list/PhoneList';
+import { deduplicatePhones } from '@/lib/utils/utils';
 
 export default async function Home() {
   const phones = await getPhones();
-  const uniquePhones = phones.filter(
-    (phone, index, self) => index === self.findIndex((p) => p.id === phone.id)
-  );
+  const uniquePhones = deduplicatePhones(phones);
 
   return <PhoneList initialPhones={uniquePhones} />;
 }
